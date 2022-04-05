ddeeb55b9564ec8437db5479df06af613e43e563
KatRoland <katona.roland@verebelyszki.hu>
2022-04-05 13:10:48 +0200

    python weblap update 04.05

---
+++ Python/python.js
@@ -0,0 +1,17 @@
+function asd(){
+    var element = document.getElementById("egy");
+    var element2 = document.getElementById("ket");
+    if (element2.style.height > element.style.height){
+        element.style.height = element2.style.height
+    }
+    else
+    {
+        element2.style.height = element.style.height
+    }
+}
+
+function asda(){
+    var element = document.getElementById("egy");
+    var element2 = document.getElementById("ket");
+    element2.style.height = element.style.height;
+}
